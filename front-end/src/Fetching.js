b2b554d9b1cbbffe8766ff389daf8548a49a826e
AndresRicci93 <andresricci93@gmail.com>
2020-08-18 13:30:29 +0200

    add style to fetching component

--- front-end/src/Fetching.js
+++ front-end/src/Fetching.js
@@ -25,34 +25,34 @@ const Fetching = () => {
 
   return (
       
-    <div>
-      <h1>Correct Phone Numbers:</h1>
-      <ul>
-        {corrects.map((correct) => (
-          <h1 key={correct.id}>{correct.sms_phone}</h1>
-        ))}
-      </ul>
+    <div className="container">
+    <h1 style={ {color: 'green'} }>Correct Phone Numbers:</h1>
+    <ul>
+      {corrects.map((correct) => (
+        <h1 key={correct.id}>{"+ " + correct.sms_phone}</h1>
+      ))}
+    </ul>
 
-      <h1>Corrected Phone Numbers - Added +27</h1>
-      <ul>
-        {addedprefixes.map((addedprefix) => (
-          <h1 key={addedprefix.id}>{addedprefix.result}</h1>
-        ))}
-      </ul>
-      <h1>Corrected Phone Numbers - Changed prefix to +27 </h1>
-      <ul>
-        {prefixs.map((prefixmodified) => (
-          <h1 key={prefixmodified.id}>{prefixmodified.result}</h1>
-        ))}
-      </ul>
+    <h1 style={ {color: 'green'} }>Corrected Phone Numbers - Added +27</h1>
+    <ul>
+      {addedprefixes.map((addedprefix) => (
+        <h1 key={addedprefix.id}>{addedprefix.result}</h1>
+      ))}
+    </ul>
+    <h1 style={ {color: 'green'} }>Corrected Phone Numbers - Changed prefix to +27 </h1>
+    <ul>
+      {prefixs.map((prefixmodified) => (
+        <h1 key={prefixmodified.id}>{prefixmodified.result}</h1>
+      ))}
+    </ul>
 
-      <h1>Incorrect Phone Numbers:</h1>
-      <ul>
-        {incorrects.map((incorrect) => (
-          <h1 key={incorrect.id}>{incorrect.sms_phone}</h1>
-        ))}
-      </ul>
-    </div>
+    <h1 style={ {color: 'red'} }>Incorrect Phone Numbers:</h1>
+    <ul>
+      {incorrects.map((incorrect) => (
+        <h1 key={incorrect.id}>{incorrect.sms_phone}</h1>
+      ))}
+    </ul>
+  </div>
   );
 };
 
